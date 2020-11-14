fc8fb5a93bffbbc96ea5436e31a7500a787184e7
Suson Waiba <susonwaiba@gmail.com>
2020-11-14 18:57:08 +0545

    updates while monochrome

--- config.h
+++ config.h
@@ -14,18 +14,18 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
+	{"",	"memory",	30,	14},
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
+	/* {"",	"forecast",	18000,	5}, */
 	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
+	/* {"",	"nettraf",	1,	16}, */
+	{"",	"internet",	5,	4},
 	{"",	"volume",	0,	10},
-	{"",	"~/mybin/statusbar/mymic",	0,	26},
+	{"",	"~/mybin/statusbar/mymic",	60,	26},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	/* {"",	"help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
