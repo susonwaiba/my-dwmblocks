2918eb31d28d9137e43cdfbfd8a0c59bac5f70c0
Suson Waiba <susonwaiba@gmail.com>
2020-11-14 19:17:35 +0545

    added space to left side

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "kbselect", 0, 30}, */
+	{"",	"~/mybin/statusbar/myleftspace",	0,	27},
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
